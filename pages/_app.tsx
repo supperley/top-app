9a5bb682c7cd3cc1f47331148246c9e4a0f409dd
supperley <v.bobrovko@gmail.com>
2023-07-25 21:37:29 +0300

    prepare project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
+import Head from 'next/head';
+import React from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+	return <>
+		<Head>
+			<title>MyTop - наш лучший топ</title>
+			<link rel="icon" href="/favicon.ico" />
+			<link rel="preconnect" href="https://fonts.gstatic.com" />
+			<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet" />
+		</Head>
+		<Component {...pageProps} />
+	</>;
 }
+
+export default MyApp;
