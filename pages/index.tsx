f9f860949bea5d4c365edd3fe9e99c392bbc0782
supperley <v.bobrovko@gmail.com>
2023-07-26 09:25:00 +0300

    add Button, P & Tag components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
-import { Htag } from "@/components";
+import { Button, Htag, P, Tag } from "@/components";
 
 export default function Home(): JSX.Element {
 	return (
 		<div>
 			<Htag tag='h1'>Текст</Htag>
+			<Button appearance='primary' arrow='right'>Кнопка</Button>
+			<Button appearance='ghost' arrow='down'>Кнопка</Button>
+			<P size='l'>Большой</P>
+			<P>Средний</P>
+			<P size='s'>Маленький</P>
+			<Tag size='s'>Ghost</Tag>
+			<Tag size='m' color='red'>Red</Tag>
+			<Tag size='s' color='green'>Green</Tag>
+			<Tag color='primary'>Green</Tag>
 		</div>
 	);
 }
