1f555db35d460683202022ab12345961c3a01a6a
supperley <v.bobrovko@gmail.com>
2023-07-28 22:17:24 +0300

    add grid layout

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,5 +1,5 @@
 import { LayoutProps } from './Layout.props';
-import styles from './P.module.css';
+import styles from './Layout.module.css';
 import cn from 'classnames';
 import { Header } from './Header/Header';
 import React, { FunctionComponent } from 'react';
@@ -8,14 +8,12 @@ import { Footer } from './Footer/Footer';
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
     return (
-        <>
-            <Header />
-            <div>
-                <Sidebar />
-                <div>{children}</div>
-            </div>
-            <Footer />
-        </>
+        <div className={styles.wrapper}>
+            <Header className={styles.header} />
+            <Sidebar className={styles.sidebar} />
+            <div className={styles.body}>{children}</div>
+            <Footer className={styles.footer} />
+        </div>
     );
 };
 
